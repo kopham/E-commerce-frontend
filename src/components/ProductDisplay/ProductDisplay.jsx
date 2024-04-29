3f49ba5e55fbc615469641d3f87876281b463fbf
kopham <khoi.2.pham@ucdenver.edu>
2024-04-28 21:33:47 -0600

    new updates

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from "../Assets/star_icon.png"
 import star_dull_icon from "../Assets/star_dull_icon.png"
+import { ShopContext } from '../../Context/ShopContext'
 
 const ProductDisplay = (props) => {
     const {product} = props;
+    const {addToCart} = useContext(ShopContext);
     return(
         <div className = 'productdisplay'>
             <div className = "productdisplay-left">
@@ -20,7 +22,7 @@ const ProductDisplay = (props) => {
             </div>
             <div className = "productdisplay-right">
                 <h1>{product.name}</h1>
-                <div className = "productdisplay-right-star">
+                <div className = "productdisplay-right-stars">
                     <img src = {star_icon} alt = ""/>
                     <img src = {star_icon} alt = ""/>
                     <img src = {star_icon} alt = ""/>
@@ -29,27 +31,25 @@ const ProductDisplay = (props) => {
                     <p>(122)</p>
                 </div>
                 <div className = "productdisplay-right-prices">
-                    <div className = "productdisplay-right-price-old">${product.old_price}
-                    <div className = "productdisplay-right-price-new">${product.new_price}
-                    </div>
-                    <div className = "productdisplay-right-description">
-                        A light weight, pullover shirt, closed fitting 
-                    </div>
-                    <div className = "productdisplay-right-size">
-                        <h1>Select Size</h1>
-                        <div className = "productdisplay-right-size">
-                            <div>S</div>
-                            <div>M</div>
-                            <div>L</div>
-                            <div>XL</div>
-                            <div>XXL</div>
-                        </div>
-                    </div>
-                    <button>ADD TO CART</button>
-                    <p className = 'prodictdisplay-right-category'><span>Category : </span> Women, T-Shirt, Crop Top</p>
-                    <p className = 'prodictdisplay-right-category'><span>Tags : </span> Modern, Latest </p>
+                    <div className = "productdisplay-right-price-old">${product.old_price}</div>
+                    <div className = "productdisplay-right-price-new">${product.new_price}</div>
+                </div>
+                <div className = "productdisplay-right-description">
+                A light weight, pullover shirt, closed fitting 
+                </div>
+                <div className = "productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className = "productdisplay-right-sizes">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
                     </div>
                 </div>
+                <button onClick = {() => {addToCart(product.id)}}>ADD TO CART</button>
+                <p className = 'prodictdisplay-right-category'><span>Category : </span> Women, T-Shirt, Crop Top</p>
+                <p className = 'prodictdisplay-right-category'><span>Tags : </span> Modern, Latest </p>
             </div>
         </div>
     )
